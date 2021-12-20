eaeb95c44c54a64c9c34262d544a24cdc122cf15
Jarome <jarome.developer@gmail.com>
2021-12-20 00:38:38 -0600

    Upload storageProcedure and README.md

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # entrevista-app
+# Jorge A. Rodriguez Méndez
+# jarome.developer@gmail.com
+# 19 de diciembre del 2021
 App Demo 
+# Para poder ejecutar la aplicación, se requiere de los siguientes pasos:
+# 1.- Descargar el proyecto o clonarlo
+# 2.- Desde una terminal, ejecutar composer install
+# 3.- Ejecutar las migraciones para tener una base de datos:
+# 4.- php artisan db:seed --class=lessonSeeder
+# 5.- php artisan db:seed --class=studentsSeeder
+# 6.- Ejecutar el script stores.sql que se encuentra dentro de la carpeta
+# 7.- Ejecutar el comando php artisan serve
+
+
+
+# Notas: Se realizo una modificación a la variable $namespace para evitar un conflicto en el llamado de los namespace
+
+# Desde un entorno linux o MacOSX, revisar que tiene permisos para poder ejecutar los comandos.
+
+# Route Service Provider
+# protected $namespace = 'App\Http\Controllers';
--- stores.sql
+++ stores.sql
@@ -1,13 +1,26 @@
+DELIMITER $$
+-- listado para asignar materias
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_list_assigned`(IN `pid` INT(11))
+BEGIN
+	
 
--- Listado de materias No seleccionadas
-DROP PROCEDURE sp_list_except_assign_qry;
-DELIMITER //
-CREATE PROCEDURE sp_list_except_assign_qry(
+        SELECT students_lessons.lesson_id, lessons.name 
+    	FROM students
+    	INNER JOIN students_lessons ON students_lessons.student_id = students.id
+INNER JOIN lessons ON lessons.id = students_lessons.lesson_id
+WHERE students.id = pid;
+
+END$$
+DELIMITER ;
+
+DELIMITER $$
+-- listado donde muestra las materias asignadas
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_list_except_assign_qry`(
     IN pid INT(11)
 )
 BEGIN
 	
-	SELECT lessons.id
+	SELECT lessons.id, lessons.name
     FROM lessons
     WHERE lessons.id NOT IN
     ( 
@@ -17,21 +30,22 @@ BEGIN
     	WHERE students.id = pid
     );
     
-END//
+END$$
+DELIMITER ;
 
-
--- Listado de materias ya seleccionadas
-DROP PROCEDURE sp_list_assigned;
-DELIMITER //
-CREATE PROCEDURE sp_list_assigned(
-    IN pid INT(11)
+DELIMITER $$
+-- Nueva relación de lessiones - estudiante 
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_save_relationship_lessons_qry`(
+	IN pid INT(11),
+    IN plesson INT(11)
 )
 BEGIN
-	
-
-        SELECT students_lessons.lesson_id 
-    	FROM students
-    	INNER JOIN students_lessons ON students_lessons.student_id = students.id
-    	WHERE students.id = pid
-
-END//
+	INSERT INTO students_lessons(lesson_id, student_id, created_at, updated_at) VALUES (plesson, pid, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+	SELECT LAST_INSERT_ID() INTO @id;
+    IF(@id > 0) THEN
+    	SELECT 'Se inserto correctamente' AS 'Mensaje'; 
+    ELSE
+    	SELECT 'Se genero un error' AS 'Mensaje'; 
+    END IF;
+END$$
+DELIMITER ;
